ef1a6c62fe398e23a3d1f9e01b4efb2bb074a5b9
Kaloyan Valentinov Dimitrov <kaloyandimitrow@gmail.com>
2019-03-10 14:03:54 +0200

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+		[Route("customers")]
+        public ActionResult Customers()
+        {
+			var vm = new CustomersViewModel
+			{
+				Customers = GenerateCustomers()
+			};
+            return View(vm);
+        }
+
+		[Route("customers/details/{id}")]
+		public ActionResult Details(int id)
+		{
+			var customer = GenerateCustomers().Where(c => c.Id == id);
+
+			if (customer.Count() == 0)
+			{
+				return HttpNotFound();
+			}
+			else
+			{
+				return View(customer.First());
+			}
+		}
+
+		private List<Customer> GenerateCustomers()
+		{
+			var customers = new List<Customer>
+			{
+				new Customer { Id = 1, Name = "John Smith"},
+				new Customer { Id = 2, Name = "Mary Williams"}
+			};
+
+			return customers;
+		}
+	}
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+	public class MoviesController : Controller
+	{
+		// GET: Movies/Random
+		public ActionResult Random()
+		{
+			var movie = new Movie { Name = "Shrek" };
+
+			var customers = new List<Customer>
+			{
+				new Customer { Name = "Customer 1"},
+				new Customer { Name = "Customer 2"}
+			};
+
+			var vm = new RandomMovieViewModel
+			{
+				Movie = movie,
+				Customers = customers
+			};
+
+			return View(vm);
+		}
+
+		//  GET: Movies/Edit/{id}
+		public ActionResult Edit(int id)
+		{
+			return Content("Edited");
+		}
+
+		//GET: Movies
+		[Route("movies")]
+		public ActionResult Movies()
+		{
+			var movies = new List<Movie>
+			{
+				new Movie {Name = "Shrek" },
+				new Movie {Name = "Wall-e" }
+			};
+
+			var vm = new MoviesViewModel
+			{
+				Movies = movies
+			};
+
+			return View(vm);
+		}
+
+		//GET: Movies/ByReleaseDate/{year}/{month}
+		[Route("movies/released/{year:regex(\\d{4}):range(1900,2019)}/{month:regex(\\d{2}):range(1,12)}")]
+		public ActionResult ByReleaseYear(int year, int month)
+		{
+			return Content(year + " | " + month);
+		}
+    }
+}
