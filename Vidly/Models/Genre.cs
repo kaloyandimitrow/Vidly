0188bd034f420892554deb3a80ad0b0e9c352493
Kaloyan Valentinov Dimitrov <kaloyandimitrow@gmail.com>
2019-03-10 15:55:32 +0200

    update Movie model with additional properties

--- Vidly/Models/Genre.cs
+++ Vidly/Models/Genre.cs
@@ -5,13 +5,9 @@ using System.Web;
 
 namespace Vidly.Models
 {
-	public class Movie
+	public class Genre
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public Genre Genre { get; set; }
-		public DateTime ReleaseDate { get; set; }
-		public DateTime AddedDate { get; set; }
-		public int NumberInStock { get; set; }
 	}
 }
