935825453a13a456aba1601c779c0d442ac06a1e
Kaloyan Valentinov Dimitrov <kaloyandimitrow@gmail.com>
2019-03-11 13:05:33 +0200

    Adding an API for customers

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+		private ApplicationDbContext _context;
+
+		public CustomersController()
+		{
+			_context = new ApplicationDbContext();					
+		}
+
+		//GET api/customers
+		public IEnumerable<Customer> GetCustomers()
+		{
+			return _context.Customers.ToList();
+		}
+
+		//GET api/customers/{id}
+		public Customer GetCustomer(int id)
+		{
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+			if (customer == null)
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+
+			return customer;
+		}
+
+		//POST api/customers
+		[HttpPost]
+		public Customer CreateCustomer(Customer customer)
+		{
+			if (!ModelState.IsValid)
+				throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+			_context.Customers.Add(customer);
+			_context.SaveChanges();
+
+			return customer;
+		}
+
+		//PUT api/customers/{id}
+		[HttpPut]
+		public void UpdateCustomer(int id, Customer customer)
+		{
+			if (!ModelState.IsValid)
+				throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+			var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+			if (customerInDB == null)
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+
+			customerInDB.Name = customer.Name;
+			customerInDB.Birthday = customer.Birthday;
+			customerInDB.MembershipTypeId = customer.MembershipTypeId;
+			customerInDB.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+			_context.SaveChanges();
+		}
+
+		//DELETE api/customer/{id}
+		[HttpDelete]
+		public void DeleteCustomer(int id)
+		{
+			var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+			if (customerInDB == null)
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+
+			_context.Customers.Remove(customerInDB);
+			_context.SaveChanges();
+		}
+	}
+}
