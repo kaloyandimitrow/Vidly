923499c52eca28cec1059cad65905b6b87905186
Kaloyan Valentinov Dimitrov <kaloyandimitrow@gmail.com>
2019-03-11 10:34:52 +0200

    Adding customer form validations

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -28,6 +28,7 @@ namespace Vidly.Controllers
 			var membershipTypes = _context.MembershipTypes.ToList();
 			var vm = new CustomerFormViewModel
 			{
+				Customer = new Customer(),
 				MembershipTypes = membershipTypes
 			};
 			return View("CustomerForm", vm);
@@ -36,6 +37,17 @@ namespace Vidly.Controllers
 		[HttpPost]
 		public ActionResult Save(Customer customer)
 		{
+			if (!ModelState.IsValid)
+			{
+				var vm = new CustomerFormViewModel
+				{
+					Customer = customer,
+					MembershipTypes = _context.MembershipTypes.ToList()
+				};
+
+				return View("CustomerForm", vm);
+			}
+
 			if (customer.Id == 0)
 			{
 				_context.Customers.Add(customer);
